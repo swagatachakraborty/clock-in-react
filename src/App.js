fc2c29bc48747a118ad8dabcf357a43eea0c6360
Swagata Chakraborty <44020073+swagatachakraborty@users.noreply.github.com>
2019-04-02 11:31:10 +0530

    implement page contain clock

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { time: new Date().toLocaleTimeString() };
+    this.interval = props.interval;
+  }
+
+  updateTime() {
+    this.setState({
+      time: new Date().toLocaleTimeString()
+    });
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.updateTime();
+    }, this.interval);
+  }
+
   render() {
+    console.log(this.state.time);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <p>Time is : </p>
+        <div>{this.state.time}</div>
       </div>
     );
   }
 }
 
-export default App;
+export default Clock;
